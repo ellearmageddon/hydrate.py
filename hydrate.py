ca07413c4909178ad85b25c86f7283f173f1df5b
ellearmageddon <35136364+ellearmageddon@users.noreply.github.com>
2018-06-13 17:43:43 -0700

    Create hydrate.py

---
+++ hydrate.py
@@ -0,0 +1,11 @@
+import time
+import webbrowser
+
+count = 0
+
+while(count < 3):
+	time.sleep(3600)
+	webbrowser.open("https://youtu.be/TwSyrOnz8lY?t=9s")
+	count = count + 1
+
+print("eat a snack and restart hydrate.py")
