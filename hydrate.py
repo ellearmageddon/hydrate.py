12867239b8864de2d3458b7cb3a220b664be6f59
ellearmageddon <35136364+ellearmageddon@users.noreply.github.com>
2018-06-14 20:39:29 -0700

    Update hydrate.py
    hydrate.py now uses a video to remind you to eat a snack, and loops three times before reminding you to stop working. Total run time of 9 hours.

--- hydrate.py
+++ hydrate.py
@@ -4,8 +4,15 @@ import webbrowser
 count = 0
 
 while(count < 3):
-	time.sleep(3600)
-	webbrowser.open("https://youtu.be/TwSyrOnz8lY?t=9s")
-	count = count + 1
 
-print("eat a snack and restart hydrate.py")
+ break_count = 0
+
+ while(break_count < 3):
+  time.sleep(3600)
+  webbrowser.open("https://youtu.be/TwSyrOnz8lY?t=9s")
+  break_count = break_count + 1
+
+ webbrowser.open("https://www.youtube.com/watch?v=hjB5gjTEEj8")
+ count = count + 1
+
+webbrowser.open("https://www.youtube.com/watch?v=Udj-o2m39NA")
